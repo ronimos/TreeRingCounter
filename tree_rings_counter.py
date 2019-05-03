8322ba25be52649525a9a0cc1fcae7a559ac75c6
ronimos <41879450+ronimos@users.noreply.github.com>
2019-05-02 21:57:19 -0600

    Added option to save rings mapping to file
    Added option to save to save rings' distance from the center in pixels unit to a csv file

--- tree_rings_counter.py
+++ tree_rings_counter.py
@@ -12,9 +12,10 @@ Click Esc to move from one screen to another.
 
 import cv2
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 x1, x2, y1, y2 = 0,0,0,0
 timg = None
@@ -34,7 +35,7 @@ def onmouse(event, x, y, flags, paprm):
     """
     global x1,x2,y1,y2, timg, draw
     if draw:
-        timg = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+        timg = img.copy()
     if event == cv2.EVENT_LBUTTONDOWN:
         draw = True
         x1, x2, y1, y2 = 0,0,0,0
@@ -99,30 +100,77 @@ def rotate_image(img, p1, p2):
     
     return rotated, rx1, ry1, rx2, ry2
     
-def count_and_show_results(rotated, rx1, ry1, rx2, ry2):
 
-    line=rotated[ry1-30: ry1+30, min(rx1,rx2):max(rx1,rx2)]
+def count_and_map_rings(img, x1, y1, x2, y2):
+    """
+    Calculates the average pixel intensities of 5 pixels along the counting line, 
+    draw rectangle of the area where the intensities are calculated and uses derivitive > 0 to idenrify ring locations
+    amd number of rings
+    Parameters:
+        img: numpy array - the image rotated to where the marked count line is horizontal 
+        x1: int - the x location at the begining of the count line on the rotated image
+        y1: int - the y location at the begining of the count line on the rotated image
+        x2: int - the x location at the end of the count line on the rotated image
+        y2: int - the x location at the end of the count line on the rotated image
+    Return:
+        line: numpy array - the image with 60 pixels around the count line by the line length
+        inensity: numpy array float (1, count line length) - average pixel intensity of 5 pixles around the count line
+        threshold: float - # of std from mean to set as threshold for ring count
+        rings: numpy array bool (1, count line length) - True for ring marker, False inbtween rings
+        count: int - number of rings in line
+    """
+    # Define image strip around the ring count line
+    line=img[ry1-30: ry1+30, min(rx1,rx2):max(rx1,rx2)]
     gray = cv2.cvtColor(line[30:35,:,:], cv2.COLOR_BGR2GRAY)
     intensity = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 1)
     line = cv2.rectangle(line,(0,30),(line.shape[1],35), (0,0,0), 1)
     intensity=intensity.mean(axis=0)
+    threshholds = intensity.mean()-intensity.std() * 1.0
+    rings = (intensity<threshholds).astype(int)
+    count = (np.diff(rings)>0).sum()
+    
+    return line, intensity, threshholds, rings, count
+
+def plot_map_rings(img, intensity, threshholds, rings, count):
+    """
+    Show the image section aroung the ring count line and plot pixel intensity along the ring count line and ring
+    locatuons below.
+    Parameters:
+        img: numpy array - the image with 60 pixels around the count line by the line length
+        inensity: numpy array float (1, count line length) - average pixel intensity of 5 pixles around the count line
+        threshold: float - # of std from mean to set as threshold for ring count
+        rings: numpy array bool (1, count line length) - True for ring marker, False inbtween rings
+        count: int - number of rings in img
+    Return:
+        None
+    """
     fig, (axim, axin, axring) = plt.subplots(nrows=3, ncols=1, figsize=(15,8))
-    axim.imshow(line[...,::-1], aspect='auto') 
-    axim.set_title('Cross section of the tree trunk where the ring are counted')
-    axim.axis('off')
+    axim.imshow(img[...,::-1], aspect="auto") 
+    axim.set_title("Cross section of the tree trunk where the ring are counted")
+    axim.axis("off")
     axim.margins(0)
     axin.plot(intensity)
-    axin.set_title('Average pixle intencity on the y axis inside the black box')
-    threshholds = intensity.mean()-intensity.std() * 1.0
-    axin.hlines(threshholds, xmax=-1, xmin=len(intensity), colors='r')
+    axin.set_title("Average pixle intencity on the y axis inside the black box")
+    axin.hlines(threshholds, xmax=-1, xmin=len(intensity), colors="r")
     axin.margins(0)
-    ring = (intensity<threshholds).astype(int)
-    axring.plot(ring)
-    count = (np.diff(ring)>0).sum()
-    axring.set_title('Ring markers, total ring count is {}'.format(count))
+    axring.plot(rings)
+    axring.set_title("Ring markers, total ring count is {}".format(count))
     axring.margins(0)
     plt.tight_layout()    
+
+def save_rings_map(rings):
     
+    Tk().withdraw()
+    save_file = asksaveasfilename(filetypes=[("Comma Separated Values", "*.csv")]) + ".csv"
+    if save_file != ".csv":
+        rings_map = (np.diff(rings)>0)  
+        df = pd.DataFrame({"pixels from center":list(range(rings_map.shape[0])), "rings": rings_map})
+        df = df[df["rings"]==True]
+        df = df[["pixels from center"]]
+        df["ring id"] = list(range(df.shape[0]))
+        df.to_csv(save_file, index=False)
+    
+
     
 # Select image and read it
 Tk().withdraw()
@@ -130,23 +178,22 @@ img_file = askopenfilename(filetypes = (("jpeg files","*.jpg"),("all files","*.*
 img = cv2.imread(img_file)
 
 # Set the window and mouse event for line draw with a mouse
-cv2.namedWindow('Tree rings', cv2.WINDOW_KEEPRATIO | cv2.WINDOW_NORMAL)
-cv2.setMouseCallback('Tree rings', onmouse)
+cv2.namedWindow("Tree rings", cv2.WINDOW_NORMAL)
+cv2.setMouseCallback("Tree rings", onmouse)
 
-
-timg = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-
+timg = img.copy()
 # Wait for a line to be drawn and update the image with a new line every time the  mouse moves
 while True:
-    cv2.imshow('Tree rings', timg)
+    cv2.imshow("Tree rings", timg)
     k = cv2.waitKey(30)
     if k & 0xFF==27:
         cv2.destroyAllWindows()
         break
-if (x1,y1)!=(x2,y2):
-    timg = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
     
+if (x1,y1)!=(x2,y2):
+    timg = img.copy()
     # Rotate the image so the drawn line will be horizontal for easy calculations
     rotated, rx1, ry1, rx2, ry2 = rotate_image(timg, (x1,y1), (x2,y2))
+    img_strip, intensity, threshholds, rings_map, rings_count = count_and_map_rings(rotated, rx1, ry1, rx2, ry2)
+    plot_map_rings(img_strip, intensity, threshholds, rings_map, rings_count)
     
-    count_and_show_results(rotated, rx1, ry1, rx2, ry2)
